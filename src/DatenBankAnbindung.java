b24ece0270957e6d9560110a6313c4ecc7cda278
tessaimrich <t.imrich@hotmail.de>
2025-01-13 15:22:02 +0100

    Doch DatenBankAnbindung Klasse: Single Responsibility Principle erfüllen, Connection Methode ohne static, graue Kommentare von Java selber entfernt

--- src/DatenBankAnbindung.java
+++ src/DatenBankAnbindung.java
@@ -2,10 +2,25 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
 public class DatenBankAnbindung {
 
+    private static final String URL="jdbc:mysql://localhost:3306/kinderarztpraxis";
+    private static final String USER="root";
+    private static final String PASSWORD="DatenForever2";
 
+    // Privater Konstruktor, um Instanziierung außerhalb der Klasse zu verhindern
+    private DatenBankAnbindung() {}
 
-
+    public Connection coni(){
+        Connection con=null;
+        try{
+            con = DriverManager.getConnection(URL,USER,PASSWORD);
+            System.out.println("Conncected to database");
+        }catch(
+                SQLException e){
+            e.printStackTrace();
+            System.out.println("Conncection failed");
+        }
+        return con;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,9 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    private static final String URL="jdbc:mysql://localhost:3306/kinderarztpraxis";
-    private static final String USER="root";
-    private static final String PASSWORD="DatenForever2";
-
     public static void main(String[] args) {
-        coni();
-    }
 
-    public static Connection coni(){
-        Connection con=null;
-        try{
-            con = DriverManager.getConnection(URL,USER,PASSWORD);
-            System.out.println("Conncected to database");
-        }catch(
-                SQLException e){
-            e.printStackTrace();
-            System.out.println("Conncection failed");
+        //usage für DatenBankAnbindung Methode von der DatenBankAnbindung Klasse
+        DatenBankAnbindung dbAnbindung = new DatenBankAnbindung();
+        dbAnbindung.coni(); // Verbindung zur Datenbank wird getestet
+            }
         }
-        return con;
-    }
-}
