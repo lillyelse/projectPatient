174559eee647da83b1340078b2c1c93173e627c7
tessaimrich <t.imrich@hotmail.de>
2025-01-25 19:11:39 +0100

    JPanel,JTextField und Konstruktor erstellt

--- src/GUI/KontaktFormular.java
+++ src/GUI/KontaktFormular.java
@@ -4,5 +4,12 @@ import javax.swing.*;
 
 public class KontaktFormular extends JPanel {
 
+    private JPanel kontaktFormularPanel;
+    private JTextField vornameField, nachnameField, geburtsdatumField, strasseField, plzField, ortField,
+            bundeslandField, geschlechtField, krankenkasseField, angehoerigerField;
 
+
+    public KontaktFormular() {
+
+    }
 }
