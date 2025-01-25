43c29031245fcb40d940221b7e01c59dcae13268
tessaimrich <t.imrich@hotmail.de>
2025-01-25 18:09:54 +0100

    Login import statement hinzugefügt

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
-import javax.swing.*;
 
+import GUI.Login;
 public class Main {
 
     public static void main(String[] args) {
 
+        //welcher Kuchen ist dein Lieblingskuchen??
+
         //Singleton Pattern,weil nur eine Instanz existieren darf-konstruktor privat deswegen rot
-        DatenBankAnbindung dbAnbindung = DatenBankAnbindung.getInstanz();
-       dbAnbindung.coni(); // Verbindung zur Datenbank wird getestet
+        //DatenBankAnbindung dbAnbindung = DatenBankAnbindung.getInstanz();
+      // dbAnbindung.coni(); // Verbindung zur Datenbank wird getestet
 
         new Login();
 
