b60dfafdf70af3d34da60d5d3c8df2decacbecb8
lisap <poehllisa@gmail.com>
2025-01-25 16:22:58 +0100

    Klasse "DeletePatientFromMenu": Attribut Patientendatenbank, Konstruktor und Methode execute()

--- src/GUI/Patient/DeletePatientFromMenu.java
+++ src/GUI/Patient/DeletePatientFromMenu.java
@@ -1,8 +1,33 @@
 package GUI.Patient;
 
+import GUI.Patientendatenbank;
+import javax.swing.*;
+
+
 public class DeletePatientFromMenu {
 
+    // Attribut
+    private Patientendatenbank patientenDatenbank;
+
+    // Konstruktor
+    public DeletePatientFromMenu(Patientendatenbank patientenDatenbank) {
+        this.patientenDatenbank = patientenDatenbank;
+    }
 
 
+    // Methode
+    public void execute(JFrame parent) {
+        String patientIdStr = JOptionPane.showInputDialog(parent, "Geben Sie die PatientID ein:");
 
+        if (patientIdStr != null) {
+            try {
+                int patientId = Integer.parseInt(patientIdStr);
+                if (patientenDatenbank.deletePatient(patientId)) {
+                    JOptionPane.showMessageDialog(parent, "Person.Patient erfolgreich gelöscht!");
+                }
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(parent, "Fehler: " + e.getMessage());
+            }
+        }
+    }
 }
