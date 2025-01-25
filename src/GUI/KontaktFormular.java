b804ca010f9a8d92e8d3520f80aa3295ad73b944
tessaimrich <t.imrich@hotmail.de>
2025-01-25 19:31:09 +0100

    Layout für JPanel erstellt inklsuive JTextFields für die ganzen Spaltennamen, die mein Patient als Attribute hat und mit formPanel.add werden Labels und unsere JTextFields zum Kontakformular hinzugefügt

--- src/GUI/KontaktFormular.java
+++ src/GUI/KontaktFormular.java
@@ -1,6 +1,7 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class KontaktFormular extends JPanel {
 
@@ -10,6 +11,48 @@ public class KontaktFormular extends JPanel {
 
 
     public KontaktFormular() {
+        this.kontaktFormularPanel = new JPanel(new BorderLayout());
 
+
+        // Initialisiere das Kontaktformular Panel
+        JPanel kontaktFormularPanel = new JPanel(new BorderLayout());
+        kontaktFormularPanel.setBorder(BorderFactory.createTitledBorder("Kontaktformular"));
+
+        // Panel für das Formular
+        JPanel formPanel = new JPanel(new GridLayout(10, 2, 10, 10));
+
+        // Felder für die Eingabe erstellen
+        vornameField = new JTextField();
+        nachnameField = new JTextField();
+        geburtsdatumField = new JTextField();
+        strasseField = new JTextField();
+        plzField = new JTextField();
+        ortField = new JTextField();
+        bundeslandField = new JTextField();
+        geschlechtField = new JTextField();
+        krankenkasseField = new JTextField();
+        angehoerigerField = new JTextField();
+
+        // mit formPanel.add Labels und Felder zum Formular hinzugefügt
+        formPanel.add(new JLabel("Vorname:"));
+        formPanel.add(vornameField);
+        formPanel.add(new JLabel("Nachname:"));
+        formPanel.add(nachnameField);
+        formPanel.add(new JLabel("Geburtsdatum (YYYY-MM-DD):"));
+        formPanel.add(geburtsdatumField);
+        formPanel.add(new JLabel("Straße:"));
+        formPanel.add(strasseField);
+        formPanel.add(new JLabel("PLZ:"));
+        formPanel.add(plzField);
+        formPanel.add(new JLabel("Ort:"));
+        formPanel.add(ortField);
+        formPanel.add(new JLabel("Bundesland:"));
+        formPanel.add(bundeslandField);
+        formPanel.add(new JLabel("Geschlecht ID:"));
+        formPanel.add(geschlechtField);
+        formPanel.add(new JLabel("Krankenkasse:"));
+        formPanel.add(krankenkasseField);
+        formPanel.add(new JLabel("Angehörige ID:"));
+        formPanel.add(angehoerigerField);
     }
 }
