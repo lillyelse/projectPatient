af6fe5df47ab1bbd47799d880789f5acebf1dad4
lisap <poehllisa@gmail.com>
2025-01-25 17:39:10 +0100

    Klasse "SearchPatientFromMenu": Methoden searchPatient() und execute() erstellt

--- src/GUI/Patient/SearchPatientFromMenu.java
+++ src/GUI/Patient/SearchPatientFromMenu.java
@@ -13,4 +13,27 @@ public class SearchPatientFromMenu {
     }
 
 
+    //METHODEN
+
+    public void searchPatient() {
+        String patientIdStr = JOptionPane.showInputDialog(null, "Geben Sie die PatientID ein:");
+        if (patientIdStr != null && !patientIdStr.isEmpty()) {
+            try {
+                int patientId = Integer.parseInt(patientIdStr);
+                Patient patient = patientenDatenbank.getPatientById(patientId);
+                if (patient != null) {
+                    JOptionPane.showMessageDialog(null, "Gefundener Person.Patient: " + patient);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Kein Person.Patient mit dieser ID gefunden!");
+                }
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Ungültige ID eingegeben. Bitte geben Sie eine gültige Zahl ein.");
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Eingabe darf nicht leer sein.");
+        }
+    }
+
+    public void execute(HauptGUI hauptGUI) {
+    }
 }
