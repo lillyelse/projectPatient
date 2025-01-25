7794a21735ab35a967632ff2d76f81bcf778d365
lisap <poehllisa@gmail.com>
2025-01-25 17:15:44 +0100

    Klasse "EditPatientFromMenu" erstellt mit Attribut und Konstruktor

---
+++ src/GUI/Patient/EditPatientFromMenu.java
@@ -0,0 +1,17 @@
+package GUI.Patient;
+
+import GUI.Patientendatenbank;
+import models.Patient;
+import javax.swing.*;
+
+public class EditPatientFromMenu {
+
+    //ATTRIBUTE
+    private Patientendatenbank patientenDatenbank;
+
+    //KONSTRUKTOR
+    public EditPatientFromMenu(Patientendatenbank patientenDatenbank) {
+        this.patientenDatenbank = patientenDatenbank;
+    }
+
+}
