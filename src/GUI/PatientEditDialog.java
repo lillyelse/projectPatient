94ec83e57b8a5adb2ed3ee403ab59329bb5a27f4
tessaimrich <t.imrich@hotmail.de>
2025-01-25 21:50:10 +0100

    Attribute/Objekte und Konstruktor erstellt-Funktion noch nicht vorhanden

--- src/GUI/PatientEditDialog.java
+++ src/GUI/PatientEditDialog.java
@@ -1,7 +1,18 @@
 package GUI;
 
-public class PatientEditDialog {
+import models.Patient;
 
+import javax.swing.*;
 
+public class PatientEditDialog extends JDialog {
 
+    private JTextField vornameField, nachnameField, geburtsdatumField, strasseField, plzField, ortField,
+            bundeslandField, geschlechtField, krankenkasseField, angehoerigerField;
+    private Patient patient;
+    private Patientendatenbank patientenDatenbank;
+    private HauptGUI hauptGUI;
+
+    public PatientEditDialog() {
+
+    }
 }
