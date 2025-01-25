c16ea8b0a0016daeb2a070a4b2b533e7ddb2da81
lisap <poehllisa@gmail.com>
2025-01-25 17:35:51 +0100

    Klasse "SearchPatientFromMenu erstellt + Attribute und Konstruktor

---
+++ src/GUI/Patient/SearchPatientFromMenu.java
@@ -0,0 +1,16 @@
+package GUI.Patient;// GUI.Patient.SearchPatientFromMenu.java
+import GUI.HauptGUI;
+import GUI.Patientendatenbank;
+import models.Patient;
+import javax.swing.*;
+
+public class SearchPatientFromMenu {
+
+    private Patientendatenbank patientenDatenbank;
+
+    public SearchPatientFromMenu(Patientendatenbank patientenDatenbank) {
+        this.patientenDatenbank = patientenDatenbank;
+    }
+
+
+}
