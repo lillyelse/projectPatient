48ee56c88bc6f09ee25895e45e53e0973dcc4b5a
lisap <poehllisa@gmail.com>
2025-01-26 10:53:56 +0100

    Javadoc für Main-Klasse erstellt

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,23 @@
 
 import GUI.Login;
+
+/**
+ * Die Main-Klasse ist der Einstiegspunkt der Anwendung.
+ */
 public class Main {
 
+    /**
+     * Der Einstiegspunkt der Anwendung.
+     * @param args Die Kommandozeilen-Argumente, die der Anwendung übergeben werden können.
+     */
     public static void main(String[] args) {
 
-        //Singleton Pattern,weil nur eine Instanz existieren darf-konstruktor privat deswegen rot
 
-
+        //Singleton Pattern,weil nur eine Instanz existieren darf- Der Konstruktor von Login ist privat
+        /**
+         * Initialisiert die Login-GUI. Es handelt sich um ein Singleton-Pattern,
+         * bei dem nur eine Instanz von Login existieren darf.
+         */
         new Login();
 
 
